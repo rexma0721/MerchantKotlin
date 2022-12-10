5369f7a4c8d57bca7b61f4a055b6e0cb691f2da8
superdevstar1218 <97291463+superdevstar1218@users.noreply.github.com>
2022-12-10 09:16:23 -0500

    update search

--- app/src/main/java/com/vroomvroom/fooddeliverys/repository/merchant/MerchantRepositoryImpl.kt
+++ app/src/main/java/com/vroomvroom/fooddeliverys/repository/merchant/MerchantRepositoryImpl.kt
@@ -15,7 +15,7 @@ import javax.inject.Inject
 class MerchantRepositoryImpl @Inject constructor(
     private val service: MerchantService,
     private val merchantMapper: MerchantMapper
-) : MerchantRepository, BaseRepository()  {
+) : MerchantRepository, BaseRepository() {
     //TODO handle error body response
     private lateinit var mFirestore: FirebaseFirestore
 
@@ -23,25 +23,25 @@ class MerchantRepositoryImpl @Inject constructor(
         val data: Resource<List<Category>>?
         mFirestore = FirebaseFirestore.getInstance()
         mFirestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
-       try {
-           val arrayList: ArrayList<Category> = ArrayList()
-           val temp: Task<QuerySnapshot>? = if(type == "search"){
-               mFirestore.collection("Category").get()
-           }else{
-               mFirestore.collection("Category").whereEqualTo("type", type).get()
-           }
-           val documents = temp!!.await().documents
-           for (document in documents) {
-               val fCategory: Category = document.toObject(Category::class.java)!!
-               fCategory.id = document.id
-               arrayList.add(fCategory)
-           }
-           data = handleSuccess(arrayList.toList())
-           onResult(data)
-       } catch (e: Exception){
-           Log.v("eerrr", e.message.toString())
-           onResult(handleException(GENERAL_ERROR_CODE))
-       }
+        try {
+            val arrayList: ArrayList<Category> = ArrayList()
+            val temp: Task<QuerySnapshot>? = if (type == "search") {
+                mFirestore.collection("Category").get()
+            } else {
+                mFirestore.collection("Category").whereEqualTo("type", type).get()
+            }
+            val documents = temp!!.await().documents
+            for (document in documents) {
+                val fCategory: Category = document.toObject(Category::class.java)!!
+                fCategory.id = document.id
+                arrayList.add(fCategory)
+            }
+            data = handleSuccess(arrayList.toList())
+            onResult(data)
+        } catch (e: Exception) {
+            Log.v("eerrr", e.message.toString())
+            onResult(handleException(GENERAL_ERROR_CODE))
+        }
     }
 
     override suspend fun getMerchants(
@@ -56,44 +56,78 @@ class MerchantRepositoryImpl @Inject constructor(
             mFirestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
             val arrayList: ArrayList<MerchantDto> = ArrayList()
             var temp: Task<QuerySnapshot>? = null
-            if(category == null && searchTerm == null){
+            if (category == null && searchTerm == null) {
                 temp = mFirestore.collection("Merchants").get()
             }
-            if(category != null){
-                temp = mFirestore.collection("Merchants").whereArrayContains("categories", category).get()
+            if (category != null) {
+                temp = mFirestore.collection("Merchants").whereArrayContains("categories", category)
+                    .get()
             }
-            if(searchTerm != null) {
-                val te = mFirestore.collection("Category").whereEqualTo("name", searchTerm).get()
-                val teresult = te.await().documents
-                temp = if(teresult.size > 0) {
-                    mFirestore.collection("Merchants").whereArrayContains("categories", searchTerm).get()
-                } else {
-                    mFirestore.collection("Merchants").orderBy("name").startAt(searchTerm)
-                        .endAt(searchTerm + "\uf8ff").get()
-                }
+            if (searchTerm != null) {
+                temp = mFirestore.collection("Merchants").get()
             }
             val documents = temp!!.await().documents
-                for (document in documents) {
-                    val fmerchant: MerchantDto = document.toObject(MerchantDto::class.java)!!
-                    val results = document.get("favorites") as List<String>
-                    val uid = if(FirebaseAuth.getInstance().currentUser != null) {FirebaseAuth.getInstance().currentUser!!.uid} else{null}
-                    fmerchant.favorite = if(uid == null){false} else{results.contains(uid)}
-                    val tde = mFirestore.collection("Merchants").document(document.id).collection("reviews").get()
-                    val tresults = tde.await().documents
-                    var sum: Int = 0;
-                    for(result in tresults) {
-                        sum += result.getLong("rate")!!.toInt()
+            for (document in documents) {
+                val fmerchant: MerchantDto = document.toObject(MerchantDto::class.java)!!
+                if (searchTerm != null) {
+                    val checkflag: Boolean = checkcontainflag(fmerchant, searchTerm)
+                    if (!checkflag) {
+                        continue
                     }
-                    fmerchant.rates = tresults.size
-                    fmerchant.ratings = if(tresults.size == 0){0.0} else{sum.toDouble()/ tresults.size}
-                    arrayList.add(fmerchant)
                 }
-                val merchants = merchantMapper.mapToDomainModelList(arrayList)
-                onResult(handleSuccess(merchants))
+                val results = document.get("favorites") as List<String>
+                val uid = if (FirebaseAuth.getInstance().currentUser != null) {
+                    FirebaseAuth.getInstance().currentUser!!.uid
+                } else {
+                    null
+                }
+                fmerchant.favorite = if (uid == null) {
+                    false
+                } else {
+                    results.contains(uid)
+                }
+                val tde =
+                    mFirestore.collection("Merchants").document(document.id).collection("reviews")
+                        .get()
+                val tresults = tde.await().documents
+                var sum: Int = 0;
+                for (result in tresults) {
+                    sum += result.getLong("rate")!!.toInt()
+                }
+                fmerchant.rates = tresults.size
+                fmerchant.ratings = if (tresults.size == 0) {
+                    0.0
+                } else {
+                    sum.toDouble() / tresults.size
+                }
+                arrayList.add(fmerchant)
+            }
+            val merchants = merchantMapper.mapToDomainModelList(arrayList)
+            onResult(handleSuccess(merchants))
         } catch (e: Exception) {
             Log.e(TAG, "Error1: ${e.message}")
             onResult(handleException(GENERAL_ERROR_CODE))
         }
+    }
+
+    private fun checkcontainflag(fmerchant: MerchantDto, searchTerm: String): Boolean {
+        if(fmerchant.name.toLowerCase().contains(searchTerm.toLowerCase())){
+            return true
+        }
+        if(fmerchant.categories.any { it.contains(searchTerm, ignoreCase = true) }){
+            return true
+        }
+        for(productSections in fmerchant.product_sections!!){
+            for(product in productSections.products){
+                if(product.name.contains(searchTerm, ignoreCase = true)){
+                    return true
+                }
+                if(product.description!!.contains(searchTerm, ignoreCase = true)){
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     override suspend fun getMerchant(id: String): Resource<Merchant>? {
@@ -106,25 +140,41 @@ class MerchantRepositoryImpl @Inject constructor(
             val document = temp.await()
             merchantDto = document.toObject(MerchantDto::class.java)!!
             val fresults = document.get("favorites") as List<String>
-            val uid = if(FirebaseAuth.getInstance().currentUser != null) {FirebaseAuth.getInstance().currentUser!!.uid} else{null}
-            merchantDto.favorite = if(uid == null){false} else{fresults.contains(uid)}
-            val tde = mFirestore.collection("Merchants").document(document.id).collection("reviews").get()
+            val uid = if (FirebaseAuth.getInstance().currentUser != null) {
+                FirebaseAuth.getInstance().currentUser!!.uid
+            } else {
+                null
+            }
+            merchantDto.favorite = if (uid == null) {
+                false
+            } else {
+                fresults.contains(uid)
+            }
+            val tde =
+                mFirestore.collection("Merchants").document(document.id).collection("reviews").get()
             val tresults = tde.await().documents
             var sum: Int = 0;
-            for(result in tresults) {
+            for (result in tresults) {
                 sum += result.getLong("rate")!!.toInt()
             }
             merchantDto.rates = tresults.size
-            merchantDto.ratings = if(tresults.size == 0){0.0}else{sum.toDouble()/ tresults.size}
+            merchantDto.ratings = if (tresults.size == 0) {
+                0.0
+            } else {
+                sum.toDouble() / tresults.size
+            }
             val productSectionsDto: ArrayList<ProductSectionsDto> = arrayListOf()
-            val psections = mFirestore.collection("Merchants").document(document.id).collection("product_sections").get()
+            val psections = mFirestore.collection("Merchants").document(document.id)
+                .collection("product_sections").get()
             val secresults = psections.await().documents
-            for(sresult in secresults){
-                val temp:ProductSectionsDto = sresult.toObject(ProductSectionsDto::class.java)!!
+            for (sresult in secresults) {
+                val temp: ProductSectionsDto = sresult.toObject(ProductSectionsDto::class.java)!!
                 val products: ArrayList<ProductDto> = arrayListOf()
-                val productsde = mFirestore.collection("Merchants").document(document.id).collection("product_sections").document(sresult.id).collection("products").get()
+                val productsde = mFirestore.collection("Merchants").document(document.id)
+                    .collection("product_sections").document(sresult.id).collection("products")
+                    .get()
                 val proresults = productsde.await().documents
-                for(proresult in proresults){
+                for (proresult in proresults) {
                     products.add(proresult.toObject(ProductDto::class.java)!!)
                 }
                 temp.products = products.toList()
@@ -132,9 +182,10 @@ class MerchantRepositoryImpl @Inject constructor(
             }
             merchantDto.product_sections = productSectionsDto.toList()
             val reviewDto: ArrayList<ReviewDto> = arrayListOf()
-            val reviews = mFirestore.collection("Merchants").document(document.id).collection("reviews").get()
+            val reviews =
+                mFirestore.collection("Merchants").document(document.id).collection("reviews").get()
             val sreviews = reviews.await().documents
-            for(sreview in sreviews){
+            for (sreview in sreviews) {
                 reviewDto.add(sreview.toObject(ReviewDto::class.java)!!)
             }
             merchantDto.reviews = reviewDto.toList()
@@ -152,20 +203,27 @@ class MerchantRepositoryImpl @Inject constructor(
         mFirestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
         val data: Resource<List<Merchant>>?
         try {
-            val temp = mFirestore.collection("Merchants").whereArrayContains("favorites", FirebaseAuth.getInstance().currentUser!!.uid).get()
+            val temp = mFirestore.collection("Merchants")
+                .whereArrayContains("favorites", FirebaseAuth.getInstance().currentUser!!.uid).get()
             val tempresult = temp.await().documents
             val merchantDto: ArrayList<MerchantDto> = arrayListOf()
-            for(document in tempresult){
+            for (document in tempresult) {
                 val fmerchant: MerchantDto = document.toObject(MerchantDto::class.java)!!
                 fmerchant.favorite = true
-                val tde = mFirestore.collection("Merchants").document(document.id).collection("reviews").get()
+                val tde =
+                    mFirestore.collection("Merchants").document(document.id).collection("reviews")
+                        .get()
                 val tresults = tde.await().documents
                 var sum: Int = 0;
-                for(result in tresults) {
+                for (result in tresults) {
                     sum += result.getLong("rate")!!.toInt()
                 }
                 fmerchant.rates = tresults.size
-                fmerchant.ratings = if(tresults.size == 0) {0.0} else{sum.toDouble()/ tresults.size}
+                fmerchant.ratings = if (tresults.size == 0) {
+                    0.0
+                } else {
+                    sum.toDouble() / tresults.size
+                }
                 merchantDto.add(fmerchant)
             }
             data = handleSuccess(merchantMapper.mapToDomainModelList(merchantDto.toList()))
@@ -184,10 +242,12 @@ class MerchantRepositoryImpl @Inject constructor(
         try {
             val temp = mFirestore.collection("Merchants").document(id).get()
             val tempresult = temp.await().get("favorites") as List<String>
-            if(tempresult.contains(uid)){
-                mFirestore.collection("Merchants").document(id).update("favorites", FieldValue.arrayRemove(uid)).await()
+            if (tempresult.contains(uid)) {
+                mFirestore.collection("Merchants").document(id)
+                    .update("favorites", FieldValue.arrayRemove(uid)).await()
             } else {
-                mFirestore.collection("Merchants").document(id).update("favorites", FieldValue.arrayUnion(uid)).await()
+                mFirestore.collection("Merchants").document(id)
+                    .update("favorites", FieldValue.arrayUnion(uid)).await()
             }
             data = handleSuccess(true)
         } catch (e: Exception) {
